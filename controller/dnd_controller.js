01e5114cca3bb3331eac0113905595cfb6c829b5
Ryan McCarthy <ryanxmccarthy@gmail.com>
2017-12-09 13:52:23 -0500

    added connection.js

--- controller/dnd_controller.js
+++ controller/dnd_controller.js
@@ -3,12 +3,13 @@ var router = express.Router();
 var burger = require("../models/dnd.js");
 
 router.get("/", function(req, res) {
-	burger.selectAll(function(data) {
-		var hbsObj = {
-			dnd: data
-		};
-		res.render("index", hbsObj);
-	});
+	// burger.selectAll(function(data) {
+	// 	var hbsObj = {
+	// 		dnd: data
+	// 	};
+	// 	res.render("index", hbsObj);
+	// });
+	res.sendFile(path.join(__dirname, '../test.html'))
 });
 
 module.exports = router;
