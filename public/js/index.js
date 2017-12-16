0036f088e41f48520f9f9323e2404b9c0997ca6a
Jane Aguero <jane.m.aguero@gmail.com>
2017-12-16 11:43:23 -0500

    fixes index.js

--- public/js/index.js
+++ public/js/index.js
@@ -25,34 +25,3 @@ $(".submit").on("click", function(class, res) {
 		$('link[href="bard.css"]').attr('href','wizard.css');
 	}
 });
-
-$("#characterForm").submit(function(event) {
-  event.preventDefault();
-  var subChara = {
-    Name : $("#charaName").val().trim(),
-    Lvl : $("#lvl").val().trim(),
-    Class: $("#class").val().trim(),
-    Race: $("#charaRace").val().trim(),
-    Background: $("#charaBackground").val().trim(),
-    Alignment: $("#charaAlign").val().trim(),
-    AC: $("#CharaAC").val().trim(),
-    XP: $("#CharaXP").val().trim(),
-    Str : $("#Str").val().trim(),
-    Dex : $("#Dex").val().trim(),
-    Con : $("#Con").val().trim(),
-    Int : $("#Int").val().trim(),
-    Wis : $("#Wis").val().trim(),
-    Cha : $("#Cha").val().trim()
-  }
-  var dataSend = $.ajax({
-    url:"/posts",
-    method: "POST",
-    data: "subChara" ,
-    dataType: "JSON"
-  })
-
-  dataSend.done(() => {
-
-    console.log(dataSend.data);
-  })
-});
