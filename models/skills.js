221fd3e730da359825cebb08bbf3d8560f3e6f7b
Benjamin Lawson <lawbe101@yahoo.com>
2017-12-16 11:14:21 -0500

    added associations and changed boolean to tinyint

--- models/skills.js
+++ models/skills.js
@@ -1,26 +1,33 @@
 module.exports = (sequelize, DataTypes) => {
   const Skills = sequelize.define('Skills', {
-  	acrobatics: DataTypes.BOOLEAN,
-  	animal_handling: DataTypes.BOOLEAN,
-  	arcana: DataTypes.BOOLEAN,
-  	athletics: DataTypes.BOOLEAN,
-  	deception: DataTypes.BOOLEAN,
-  	history: DataTypes.BOOLEAN,
-  	insight: DataTypes.BOOLEAN,
-  	intimidation: DataTypes.BOOLEAN,
-  	investigation: DataTypes.BOOLEAN,
-  	medicine: DataTypes.BOOLEAN,
-  	nature: DataTypes.BOOLEAN,
-  	perception: DataTypes.BOOLEAN,
-  	performance: DataTypes.BOOLEAN,
-  	persuasion: DataTypes.BOOLEAN,
-  	religion: DataTypes.BOOLEAN,
-  	sleight_of_hand: DataTypes.BOOLEAN,
-  	stealth: DataTypes.BOOLEAN,
-  	survival: DataTypes.BOOLEAN,
+  	acrobatics: DataTypes.TINYINT,
+  	animal_handling: DataTypes.TINYINT,
+  	arcana: DataTypes.TINYINT,
+  	athletics: DataTypes.TINYINT,
+  	deception: DataTypes.TINYINT,
+  	history: DataTypes.TINYINT,
+  	insight: DataTypes.TINYINT,
+  	intimidation: DataTypes.TINYINT,
+  	investigation: DataTypes.TINYINT,
+  	medicine: DataTypes.TINYINT,
+  	nature: DataTypes.TINYINT,
+  	perception: DataTypes.TINYINT,
+  	performance: DataTypes.TINYINT,
+  	persuasion: DataTypes.TINYINT,
+  	religion: DataTypes.TINYINT,
+  	sleight_of_hand: DataTypes.TINYINT,
+  	stealth: DataTypes.TINYINT,
+  	survival: DataTypes.TINYINT,
   	attack1: DataTypes.STRING,
   	attack2: DataTypes.STRING,
   	attack3: DataTypes.STRING
   });
+
+  Skills.associate = models => {
+    Skills.belongsTo(models.Character, {
+      onDelete: 'cascade'
+    })
+  }
+
   return Skills;
 };
