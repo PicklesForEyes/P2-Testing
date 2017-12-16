2a74de38833dd4448b565ec9e45cf469848a1e59
Ryan McCarthy <ryanxmccarthy@gmail.com>
2017-12-16 11:38:15 -0500

    added ajax file

---
+++ public/js/ajax.js
@@ -0,0 +1,31 @@
+console.log('connected')
+
+$("#submit").on('click', function(event) {
+  event.preventDefault();
+  var subChara = {
+    Name : $("#charaName").val().trim(),
+    LvlClass : $("#lvlClass").val().trim(),
+    Race: $("#charaRace").val().trim(),
+    Background: $("#charaBackground").val().trim(),
+    Alignment: $("#charaAlign").val().trim(),
+    AC: $("#CharaAC").val().trim(),
+    XP: $("#CharaXP").val().trim(),
+    Str : $("#Str").val().trim(),
+    Dex : $("#Dex").val().trim(),
+    Con : $("#Con").val().trim(),
+    Int : $("#Int").val().trim(),
+    Wis : $("#Wis").val().trim(),
+    Cha : $("#Cha").val().trim()
+  }
+
+  var dataSend = $.ajax({
+    url:"/posts",
+    method: "POST",
+    data: "subChara" ,
+    dataType: "JSON"
+  })
+
+  dataSend.done(() => {
+    console.log(dataSend.data);
+  })
+});
