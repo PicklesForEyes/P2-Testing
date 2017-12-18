390f9603c4a87cf3d26c4c0bdcf8e473d4ba2f29
Jane Aguero <jane.m.aguero@gmail.com>
2017-12-18 18:38:52 -0500

    changes to index.js, starts adding custom backgrounds

--- public/js/index.js
+++ public/js/index.js
@@ -1,27 +1,47 @@
-$(".submit").on("click", function(class, res) {
-	if ($("#class").val === "bard") {
+
+// custom css
+$(window).load(function() {
+	if ($("#class").val().trim() === "bard") {
 		$('link[href="bard.css"]').attr('href','bard.css');
-	} else if ($("#class").val === "barbarian") {
+		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/bard.jpg');
+	} else if ($("#class").val().trim() === "barbarian") {
 		$('link[href="bard.css"]').attr('href','barbarian.css');
-	} else if ($("#class").val === "cleric") {
+		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/barbarian.jpg');
+	} else if ($("#class").val().trim() === "cleric") {
 		$('link[href="bard.css"]').attr('href','cleric.css');
-	} else if ($("#class").val === "druid") {
+		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/cleric.jpg');
+	} else if ($("#class").val().trim() === "druid") {
 		$('link[href="bard.css"]').attr('href','druid.css');
-	} else if ($("#class").val === "fighter"){
+		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/druid.jpg');
+	} else if ($("#class").val().trim() === "fighter"){
 		$('link[href="bard.css"]').attr('href','fighter.css');
-	} else if ($("#class").val === "paladin"){
+		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/fighter.jpg');
+	} else if ($("#class").val().trim() === "paladin"){
 		$('link[href="bard.css"]').attr('href','paladin.css');
-	} else if ($("#class").val === "monk"){
+		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/paladin.jpg');
+	} else if ($("#class").val().trim() === "monk"){
 		$('link[href="bard.css"]').attr('href','monk.css');
-	} else if ($("#class").val === "ranger"){
+		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/monk.jpg');
+	} else if ($("#class").val().trim() === "ranger"){
 		$('link[href="bard.css"]').attr('href','ranger.css');
-	} else if ($("#class").val === "rogue"){
+		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/ranger.jpg');
+	} else if ($("#class").val().trim() === "rogue"){
 		$('link[href="bard.css"]').attr('href','rogue.css');
-	} else if ($("#class").val === "sorcerer"){
+		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/rogue.jpg');
+	} else if ($("#class").val().trim() === "sorcerer"){
 		$('link[href="bard.css"]').attr('href','sorcerer.css');
-	} else {
+		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/sorcerer.jpg');
+	} else if ($("#class").val().trim() === "wizard"){
 		$('link[href="bard.css"]').attr('href','wizard.css');
+		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/wizard.jpg');
+	} else {
+		$('link[href="bard.css"]').attr('href','style.css');
+		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/rogue.jpg');
 	}
 });
+
+//progress bars
+
+
 
 module.exports = "index";
