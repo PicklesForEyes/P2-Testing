70ed3deee1d11c4b2c2d0305bb8eb30674e7dd91
Jane Aguero <jane.m.aguero@gmail.com>
2017-12-18 18:57:10 -0500

    fixes to index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,42 +1,42 @@
 
 // custom css
 $(window).load(function() {
-	if ($("#class").val().trim() === "bard") {
+	if ($("#class").val() === "bard") {
 		$('link[href="bard.css"]').attr('href','bard.css');
-		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/bard.jpg');
-	} else if ($("#class").val().trim() === "barbarian") {
+		$('img[src="assets/images/rogue.jpg"]').attr('src', 'assets/images/bard.jpg');
+	} else if ($("#class").val() === "barbarian") {
 		$('link[href="bard.css"]').attr('href','barbarian.css');
-		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/barbarian.jpg');
-	} else if ($("#class").val().trim() === "cleric") {
+		$('img[src="assets/images/rogue.jpg"]').attr('src', 'assets/images/barbarian.jpg');
+	} else if ($("#class").val() === "cleric") {
 		$('link[href="bard.css"]').attr('href','cleric.css');
-		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/cleric.jpg');
-	} else if ($("#class").val().trim() === "druid") {
+		$('img[src="assets/images/rogue.jpg"]').attr('src', 'assets/images/cleric.jpg');
+	} else if ($("#class").val() === "druid") {
 		$('link[href="bard.css"]').attr('href','druid.css');
-		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/druid.jpg');
-	} else if ($("#class").val().trim() === "fighter"){
+		$('img[src="assets/images/rogue.jpg"]').attr('src', 'assets/images/druid.jpg');
+	} else if ($("#class").val() === "fighter"){
 		$('link[href="bard.css"]').attr('href','fighter.css');
-		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/fighter.jpg');
-	} else if ($("#class").val().trim() === "paladin"){
+		$('img[src="assets/images/rogue.jpg"]').attr('src', 'assets/images/fighter.jpg');
+	} else if ($("#class").val() === "paladin"){
 		$('link[href="bard.css"]').attr('href','paladin.css');
-		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/paladin.jpg');
-	} else if ($("#class").val().trim() === "monk"){
+		$('img[src="assets/images/rogue.jpg"]').attr('src', 'assets/images/paladin.jpg');
+	} else if ($("#class").val() === "monk"){
 		$('link[href="bard.css"]').attr('href','monk.css');
-		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/monk.jpg');
-	} else if ($("#class").val().trim() === "ranger"){
+		$('img[src="assets/images/rogue.jpg"]').attr('src', 'assets/images/monk.jpg');
+	} else if ($("#class").val() === "ranger"){
 		$('link[href="bard.css"]').attr('href','ranger.css');
-		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/ranger.jpg');
-	} else if ($("#class").val().trim() === "rogue"){
+		$('img[src="assets/images/rogue.jpg"]').attr('src', 'assets/images/ranger.jpg');
+	} else if ($("#class").val() === "rogue"){
 		$('link[href="bard.css"]').attr('href','rogue.css');
-		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/rogue.jpg');
-	} else if ($("#class").val().trim() === "sorcerer"){
+		$('img[src="assets/images/rogue.jpg"]').attr('src', 'assets/images/rogue.jpg');
+	} else if ($("#class").val() === "sorcerer"){
 		$('link[href="bard.css"]').attr('href','sorcerer.css');
-		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/sorcerer.jpg');
-	} else if ($("#class").val().trim() === "wizard"){
+		$('img[src="assets/images/rogue.jpg"]').attr('src', 'assets/images/sorcerer.jpg');
+	} else if ($("#class").val() === "wizard"){
 		$('link[href="bard.css"]').attr('href','wizard.css');
-		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/wizard.jpg');
+		$('img[src="assets/images/rogue.jpg"]').attr('src', 'assets/images/wizard.jpg');
 	} else {
 		$('link[href="bard.css"]').attr('href','style.css');
-		$('img[src="../public/assets/images/rogue.jpg"]').attr('src', '../public/assets/images/rogue.jpg');
+		$('img[src="assets/images/rogue.jpg"]').attr('src', 'assets/images/rogue.jpg');
 	}
 });
 
